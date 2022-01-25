9db948e02680ab120f4787a3494519493885c7b0
abhijha <abhijha@adobe.com>
2022-01-25 17:32:13 +0530

    Correcting CombinationSumIV_377

--- src/CombinationSumIV_377.java
+++ src/CombinationSumIV_377.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CombinationSumIV_377 {
+import java.util.Arrays;
+public class CombinationSumIV_377 {
+    public int combinationSum4(int[] nums, int target) {
+        int [] dp = new int[target+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+
+        return combinationSum(nums, target, dp);
+    }
+
+    private int combinationSum(int[] nums, int target, int [] dp) {
+        if(target < 0 || dp[target] == 0) {
+            return 0;
+        }else if(target == 0 || dp[target] > 0) {
+            return dp[target];
+        }else {
+            int count = 0;
+            for(int num : nums) {
+                count += combinationSum(nums, target-num, dp);
+            }
+
+            dp[target] = count;
+            return dp[target];
+        }
+    }
 }
